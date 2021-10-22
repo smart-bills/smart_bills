a350298c215f6b6fa13762b3e9e90853687c078e
Shiyang Chen <ana.chensy@gmail.com>
2021-10-22 01:11:37 -0400

    add home navbar and routes.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Login from './components/login';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Login  /> 
-      </header>
+    <div>
+       <Routes  /> 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Router } from 'react-router-dom';
+import history from './history';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <Router history={history}>
+        <App />
+      </Router>
+    ,
+  document.getElementById('app')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// {/* reportWebVitals(); */}
