bd0ae2e4df1eaf8436ab026a6f741ba3070d5d9c
WenxuC <wenx.chen@outlook.com>
2021-10-30 00:30:17 -0400

    delete env and secret files

--- secret.js
+++
@@ -1,3 +0,0 @@
-const dbKey = "mongodb+srv://superwoman:superwoman@smartbillsdatabase.v48nf.mongodb.net/SmartBillsDatabase?retryWrites=true&w=majority"
-const ocrKey = "d5288346b188957"
-module.exports = {dbKey , ocrKey};
