2a9ca48cf01be99b28c308357088600e591cb886
Gabriel Yan Zhen Wu <gabrielzhenwu@gmail.com>
2021-12-26 22:40:52 -0500

    Added password hashing

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,25 +1,35 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-
-router.post('/signup', async(req, res) =>{
+router.post('/signup', async(req, res) => {
     
-    const signedUpUser = await new User({
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        email:req.body.email,
-        password:req.body.password
-    })
+    const signedUpUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(signedUpUser.password, salt);
+        signedUpUser.password = hashedPassword;
+    } catch (error) {
+        throw(error);
+    }
+    
     signedUpUser.save()
-    .then(data =>{
-        res.json(data)
-    })
-    .catch(error =>{
-        res.json(error)
-    })
+        .then(data => {
+            res.json(data);
+            res.redirect('http://localhost:3000');
+        })
+        .catch(error => res.json(error))
+
 });
-router.get('/user', async(req, res) =>{
+
+router.get('/user', async(req, res) => {
     try{
         const user = await User.find()
         res.send(user)
