f8647ec38949637ca38e5ff7a2343fe42a1b1e35
Gabriel Yan Zhen Wu <gabrielzhenwu@gmail.com>
2021-12-28 17:27:18 -0500

    Added session

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
-const express    =     require('express');
-const cors       =     require('cors');
-const mongoose   =     require('mongoose');
-const session    =     require('express-session');
-const userRoute  =     require('./routes/userRoute');
-// const parseReceipt = require('./ocr');
-const {dbKey}    =     require('../secrets');
-const PORT       =     process.env.PORT || 8000
+const express       =     require('express');
+const cors          =     require('cors');
+const mongoose      =     require('mongoose');
+const session       =     require('express-session');
+const MongoStore    =     require('connect-mongo');
+const userRoute     =     require('./routes/userRoute');
+const {dbKey, sessionSecret}       =     require('../secrets');
+const PORT          =     process.env.PORT || 8000
 
 const app = express();
+// Set up all the middlewares.
 app.use(cors());
 app.use(express.json({limit: '50mb', extended: true}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24},
+    store: MongoStore.create({mongoUrl: dbKey})}));
 
 app.get('/', (req, res, next) => {
     res.send('Hi');
@@ -25,7 +32,4 @@ app.get('/', (req, res, next) => {
 // });
 
 app.use('/app', userRoute)
-app.listen(PORT, () => {
-    console.log(`Backend is connected now: ${new Date()}`);
-    mongoose.connect(dbKey, () => console.log (`Database is connected now: ${new Date()}`));
-});
+app.listen(PORT, () => console.log(`Backend is connected now: ${new Date()}`));
