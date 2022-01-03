571c26494a0b91439d88392d0aa8af2ba6b184c8
WenxuC <wenx.chen@outlook.com>
2022-01-03 12:30:44 -0500

    add copyright.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
-//import Copyright from './Copyright';
+import Copyright from './Copyright';
 
 const theme = createTheme();
 
