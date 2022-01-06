fde363cdcb78032503963c84dd41415e34b362ac
gabzn <gabrielzhenwu@gmail.com>
2022-01-05 23:31:50 -0500

    added more field for the dish model

--- server/models/dish.js
+++ server/models/dish.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const DishSchema = new Schema({
-    dishName: { type: String, required: true },
-    amount: { type: String, required: true },
-    paid: { type: Boolean},
-    userEmail: { type: String},
-    description: { type: String},
+    dishName: { type: String, required: true},
+    amount: { type: String, required: true},
+    paid: { type: Boolean, default: false},
+    userEmail: { type: String, required: true},
     date: { type: String, required: true},
     billID: { type: String, required: true},
-    storeName: { type: String, required: true}
+    storeName: { type: String, required: true},
+    description: { type: String},
 }, { timestamps: true });
 
 const Dish = mongoose.model('Dish', DishSchema);
