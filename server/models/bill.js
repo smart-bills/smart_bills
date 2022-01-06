27781cbe3c863943382054234afe456f6038b98c
gabzn <gabrielzhenwu@gmail.com>
2022-01-06 15:42:08 -0500

    Rewrote bill schema

--- server/models/bill.js
+++ server/models/bill.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const BillSchema = new Schema({
+    hostID: { type: Schema.ObjectId, required: true},
     storeName: { type: String, required: true},
     date: { type: Date, required: true},
     amount: { type: String, required: true},
     paid: { type: Boolean, default: false},
-    userEmail: String,
-    dishes: [{type: Schema.ObjectId, ref: 'Dish'}],
-    invitees: [String],
+    dishes: [{
+        user: { type: Schema.ObjectId, required: true},
+        dishName: { type: String, required: true},
+        amount: { type: String, required: true},
+        userEmail: { type: String, required: true},
+        paid: { type: Boolean, default: false},
+        date: Date,
+        storeName: String,
+        description: String
+    }],
     description: String
 }, { timestamps: true });
 
 const Bill = mongoose.model('Bill', BillSchema);
 module.exports = Bill;
+
+// dishes: [{type: Schema.ObjectId, ref: 'Dish'}],
