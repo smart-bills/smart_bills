97a07a025a6d08befc63e7603d4e1d48ce5b74b2
WenxuC <wenx.chen@outlook.com>
2022-01-11 15:09:54 -0500

    fixed the database connection issue, script now working

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
+
 const mongoose = require('mongoose');
-// const parseImage = require('./visionAPI');
-require('dotenv').config({ path: '../.env' });
-const dbKey = process.env.dbKey;
+require('dotenv').config();
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
@@ -15,23 +15,22 @@ app.get('/', (req, res, next) => {
 	res.send('Hi');
 });
 
-// app.post('/parseImage', async (req, res, next) => {
-// 	console.log('parseImage was run');
-// 	let imageInBase64 = req.body.file;
-// 	imageInBase64 = imageInBase64.split(',');
-// 	imageInBase64 = imageInBase64[1];
-
-// 	await parseImage(imageInBase64);
-// 	res.json({ message: 'Upload was successful' });
-// });
-
 app.use('/app/dashboard', require('./routes/dashboardRoute'));
 app.use('/app/register', require('./routes/registerRoute'));
 app.use('/app/auth', require('./routes/authRoute'));
 app.use('/app/bill', require('./routes/billRoute'));
 
-app.listen(PORT, () => {
-	mongoose.connect(dbKey);
-	console.log(`Backend is now connected ${Date()}`);
-	console.log(`Database is now connected ${Date()}`);
+// Connecting Database
+const dbKey = process.env.dbKey;
+mongoose.connect(dbKey, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('Connected Database Successfully');
 });
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app.listen(PORT, () => {
+// 	mongoose.connect(dbKey);
+// 	console.log(`Backend is now connected ${Date()}`);
+// 	console.log(`Database is now connected ${Date()}`);
+// });
