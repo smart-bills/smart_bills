614d6273cb609146ea40c1dd1ae03d2ddc87f4cf
gabzn <gabrielzhenwu@gmail.com>
2022-01-12 22:08:29 -0500

    Added dashboard page.

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react'
+
+function Dashboard() {
+
+    return (
+        <div>
+            Welcome back
+        </div>
+    )
+}
+
+export default Dashboard
