e14db1f313b43c511fc6dbd2463c32a4e12d0b84
gabzn <gabrielzhenwu@gmail.com>
2022-01-14 17:09:53 -0500

    some bugs in here. Needs to be fixed.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import { useNavigate, Navigate } from 'react-router-dom';
+// import {decode} from 'jsonwebtoken';
 
 function Dashboard() {
     const navigate = useNavigate();
 
+    // useEffect(() => {
+    //     const token = localStorage.getItem('token');
+    //     if(token) {
+    //         const user = decode(token);
+    //         if(!user) {
+    //             localStorage.removeItem('token');
+    //             navigate('/login');
+    //         }
+    //     }
+    // })
+
     return (
         <div>
+        
+            {/* navigate is a function that can be used to redirect user. */}
             <button onClick={() => navigate('/login')}>
                 Click to go back to the log in page.
             </button>
-        
+
+            {/* Navigate can be used to check if the user is authed. If not, use it to redirect user back. */}
+            {/* <Navigate to='/login' /> */}
+
             Welcome back
         </div>
     )
