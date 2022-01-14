b77df0477312cda1b7dc97f6b53c9828b73b63a5
gabzn <gabrielzhenwu@gmail.com>
2022-01-13 23:45:42 -0500

    Added navigate hook.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
 
 function Dashboard() {
+    const navigate = useNavigate();
 
     return (
         <div>
+            <button onClick={() => navigate('/login')}>
+                Click to go back to the log in page.
+            </button>
+        
             Welcome back
         </div>
     )
