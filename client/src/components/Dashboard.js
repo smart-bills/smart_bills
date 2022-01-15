4868c3f22ed8c5521a60ddcae941a42b38160eda
gabzn <gabrielzhenwu@gmail.com>
2022-01-15 18:21:29 -0500

    Added get bills.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,33 +1,68 @@
 import React, {useEffect, useState} from 'react';
-import { useNavigate, Navigate } from 'react-router-dom';
+// import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import axios from 'axios';
 
 function Dashboard() {
     const navigate = useNavigate();
+    const [bills, setBills] = useState();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
+        
+        // Check to see if there's a token.
+        // Then get all the bills for that user.
         if(token) {
-            const user = jwt_decode(token);
+            const {user} = jwt_decode(token);
             if(!user) {
                 localStorage.removeItem('token');
                 navigate('/login');
             }
+            
+            getBills(token, user.id);
+        } else  {
+            navigate('/login');
         }
-    })
+
+        // Query the backend and database to get all the bills.
+        async function getBills(token, userid) {
+            const url = `http://localhost:8000/app/bill/?userid=${userid}`;
+            const headers = { 'x-auth-token': token};
+            const res = await axios.get(url, { headers });
+
+            if(res.error) {
+                console.log(res.error);
+            } else {
+                console.log(res.data.bills);
+                setBills(res.data.bills);
+            }
+
+            // if(res.bills?.length === 0) {
+            //     console.log('You have no bills at the moment');
+            // } else {
+            //     setBills(...res.bills);
+            // }
+            
+        };
+
+    }, []);
 
     return (
         <div>
         
             {/* navigate is a function that can be used to redirect user. */}
-            <button onClick={() => navigate('/login')}>
+            {/* <button onClick={() => navigate('/login')}>
                 Click to go back to the log in page.
-            </button>
+            </button> */}
 
             {/* Navigate can be used to check if the user is authed. If not, use it to redirect user back. */}
             {/* <Navigate to='/login' /> */}
 
-            Welcome back
+            Welcome back!
+
+            Here is all your bills.
+            <div>{bills && bills.map(bill => bill.storeName)}</div>
         </div>
     )
 }
