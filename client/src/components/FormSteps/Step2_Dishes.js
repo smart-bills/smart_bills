0e67f17f14b92dfb9da27f4dc32cee7555f950fc
gabzn <gabrielzhenwu@gmail.com>
2022-01-22 22:12:08 -0500

    Added conditional redenring on email field.

--- client/src/components/FormSteps/Step2_Dishes.js
+++ client/src/components/FormSteps/Step2_Dishes.js
@@ -1,54 +1,56 @@
 import React from 'react';
 import {Box, TextField, Button} from '@mui/material'
 
-function Step2_dishes({dishes, onChange, handleRemoveField}) {
-  
+function Step2_dishes({dishes, changeDishInfo, removeDish, splitBy}) {
+    
+    const showEmailField = (splitBy === 'Split by Dishes' ? true : false);
+
     return (
     <React.Fragment>
-
         {dishes.map((item, index) => (
-                <Box component='div' key={index}>
+            <Box component='div' key={index}>
                                 
+                <Box component='div'>
+                    <TextField
+                        margin='dense'
+                        label="Dish Name"
+                        type='text'
+                        variant="outlined"
+                        value={item.dishName}
+                        name='dishName'
+                        onChange={e => changeDishInfo(e, index)}
+                    />
+                </Box>
+                 
+                <Box component='div'>
+                    <TextField
+                        margin='dense'
+                        label="Price"
+                        type='text'
+                        variant="outlined"
+                        value={item.amount}
+                        name='amount'
+                        onChange={e => changeDishInfo(e, index)}
+                    />
+                </Box>
+
+                {showEmailField && 
                     <Box component='div'>
                         <TextField
                             margin='dense'
-                            label="Dish Name"
+                            label="Email"
                             type='text'
                             variant="outlined"
-                            value={item.dishName}
-                            name='dishName'
-                            onChange={e => onChange(e, index)}
+                            value={item.userEmail}
+                            name='userEmail'
+                            onChange={e => changeDishInfo(e, index)}
                         />
                     </Box>
-                 
-                    <Box component='div'>
-                        <TextField
-                            margin='dense'
-                                label="Price"
-                                type='text'
-                                variant="outlined"
-                                value={item.amount}
-                                name='amount'
-                                onChange={e => onChange(e, index)}
-                        />
-                    </Box>
-                                
-                    <Box component='div'>
-                        <TextField
-                                margin='dense'
-                                label="Email"
-                                type='text'
-                                variant="outlined"
-                                value={item.userEmail}
-                                name='userEmail'
-                                onChange={e => onChange(e, index)}
-                        />
-                    </Box>
-                             
-                    <Button onClick={e => handleRemoveField(e, index)}>Remove</Button>
-		        </Box>
-	        ))}
-
+                }            
+  
+                <Button onClick={e => removeDish(e, index)}>Remove</Button>
+		    </Box>
+	    ))}
     </React.Fragment>
   )
 }
