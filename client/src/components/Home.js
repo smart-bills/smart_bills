0f7a83317bcecbe8169121fc236a81d36cdac6d2
WenxuC <wenx.chen@outlook.com>
2022-01-24 12:24:05 -0500

    UI for navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,18 +3,19 @@ import * as React from 'react';
 // import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Home() {
-  
-  //const theme = createTheme();
-  
-  return (
-    <div>
-      <div className="row">
-        <div className="logo">
-          <img src='../../home.png' height='200' alt='Home Logo'/>
-        </div>
-      </div>  
-    </div>
-  )
+	//const theme = createTheme();
+
+	return (
+		<div>
+			<nav className='navbar bg-dark'>
+				<div className='row'>
+					<div className='logo'>
+						<img src='../../home.png' height='200' alt='Home Logo' />
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 }
 
-export default Home; 
+export default Home;
