db7388ae3d57dd5d7a751866e4ce87bc9faaeefd
gabzn <gabrielzhenwu@gmail.com>
2022-01-24 23:54:14 -0500

    Added stylings to the navbar and front page.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,58 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+
 // MUI
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: teal[500]
+		}
+	}
+})
+
+const item = {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	px: 5,
+};
+
+const Root = styled('section')(({ theme }) => ({
+	color: theme.palette.common.white,
+	position: 'relative',
+	display: 'flex',
+	alignItems: 'center',
+	[theme.breakpoints.up('sm')]: {
+	  height: '80vh',
+	  minHeight: 500,
+	  maxHeight: 1300,
+	},
+}));
+  
+const Background = styled(Box)({
+	position: 'absolute',
+	left: 0,
+	right: 0,
+	top: 0,
+	bottom: 0,
+	backgroundSize: 'cover',
+	backgroundRepeat: 'no-repeat',
+	zIndex: -2,
+});
 
 function Home() {
 	const navigate = useNavigate();
-	const token = localStorage.getItem('token');
+
 	useEffect(() => {
 		const token = localStorage.getItem('token');
 
@@ -18,56 +60,143 @@ function Home() {
 			navigate('/dashboard');
 		}
 	});
-	const Img = styled('img')({
-		margin: 'auto',
-		display: 'block',
-		maxWidth: '100%',
-		maxHeight: '100%',
-	});
+
 	return (
-		<Paper
-			sx={{
-				p: 5,
-				margin: 'auto',
-				maxWidth: 600,
-				flexGrow: 1,
-				mt: 5,
-			}}
-		>
-			<Grid container spacing={2}>
-				<Grid item>
-					<Typography
-						gutterBottom
-						component='div'
-						variant='h3'
-						sx={{ textAlign: 'center' }}
-					>
+		<div>
+			<Root>
+				<Container
+					sx={{
+						mt: 3,
+						mb: 14,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center'
+					}}
+				>
+
+					<img
+						style={{display: 'none'}}
+						src={'../../home1.jpg'}
+						alt="increase priority"
+					/>
+
+					<Typography color="inherit" align="center" variant="h3" marked="center">
 						Welcome to SmartBills
 					</Typography>
-					<Img alt='home' src='../../home.png' />
-				</Grid>
-			</Grid>
-			<Grid item>
-				<Button
-					href='/login'
-					Button
-					color='inherit'
-					variant='outlined'
-					sx={{ mt: 1 }}
-				>
-					Login
-				</Button>
-				<Button
-					href='/signup'
-					Button
-					color='inherit'
-					variant='outlined'
-					sx={{ mt: 1, mx: 1 }}
-				>
-					Sign up
-				</Button>
-			</Grid>
-		</Paper>
+
+					<Typography
+						color="inherit"
+						align="center"
+						variant="h5"
+						sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+					>
+						SmartBills allows you to manage and split your restaurant bills more easily. 
+					</Typography>
+					
+					<ThemeProvider theme={theme}>
+						<Button
+							color="primary"
+							variant="contained"
+							size="large"
+							component="a"
+							href="/signup"
+							sx={{ minWidth: 200 }}
+						>
+							Try It Out
+						</Button>
+					</ThemeProvider>
+
+					<Box
+						sx={{
+							position: 'absolute',
+							left: 0,
+							right: 0,
+							top: 0,
+							bottom: 0,
+							backgroundColor: 'common.black',
+							opacity: 0.5,
+							zIndex: -1,
+						}}
+					/>
+					
+					<Background sx={{
+							backgroundImage: `url(${'../../home1.jpg'})`,
+							backgroundPosition: 'center'}} 
+					/>
+
+				</Container>
+			</Root>
+
+			<Box component="section" sx={{ display: 'flex', 
+										   overflow: 'hidden'}}
+    		>
+				<Container sx={{ mt: 15, mb: 20, display: 'flex', position: 'relative' }}>
+					<Box
+						component="img"
+						src="../../productCurvyLines.png"
+						alt="curvy lines"
+						sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
+					/>
+					<Grid container spacing={5}>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+						<Box
+							component="img"
+							src="../../easeofuse.png"
+							alt="easeofuse"
+							sx={{ height: 55 }}
+						/>
+						<Typography variant="h6" sx={{ my: 5 }}>
+							Ease of Use
+						</Typography>
+						<Typography variant="h5">
+							{
+								'Using SmartBills is easy. You just need to add a bill and fill out the info of the bill.'
+							}
+						</Typography>
+						</Box>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+						<Box
+							component="img"
+							src="../../split.png"
+							alt="split"
+							sx={{ height: 55 }}
+						/>
+						<Typography variant="h6" sx={{ my: 5 }}>
+							Split the Bill in Multiple Way
+						</Typography>
+						<Typography variant="h5">
+							{
+								'You can decide if you want to go dutch or pay equally with your friends.'
+							}
+						</Typography>
+						</Box>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Box sx={item}>
+						<Box
+							component="img"
+							src="../../email.png"
+							alt="clock"
+							sx={{ height: 55 }}
+						/>
+						<Typography variant="h6" sx={{ my: 5 }}>
+							Email Reminder
+						</Typography>
+						<Typography variant="h5">
+							{
+								'By sending out an email to your friends, you can avoid asking them to pay face to face.'
+							}
+						</Typography>
+						</Box>
+					</Grid>
+					</Grid>
+				</Container>
+    		</Box>
+
+		</div>
 	);
 }
 
