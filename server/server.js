3d93bf6d9e479c4d5f0e5e2788d87070ac37c883
WenxuC <wenx.chen@outlook.com>
2022-01-29 23:50:50 -0500

    change the path for deploy

--- server/server.js
+++ server/server.js
@@ -28,8 +28,12 @@ connection.once('open', () => {
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '..', 'client/public/index.html'));
+	// app.get('*', (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	// });
+	app.get('*', function (req, res) {
+		const index = path.join(__dirname, 'build', 'index.html');
+		res.sendFile(index);
 	});
 }
 
