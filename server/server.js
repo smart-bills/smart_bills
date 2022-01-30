6701b2a975428487b20cc30500ff487166fae72a
WenxuC <wenx.chen@outlook.com>
2022-01-29 23:05:06 -0500

    scripts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
@@ -14,6 +15,15 @@ app.use('/app/dashboard', require('./routes/dashboardRoute'));
 app.use('/app/register', require('./routes/registerRoute'));
 app.use('/app/auth', require('./routes/authRoute'));
 app.use('/app/bill', require('./routes/billRoute'));
+
+// Server static assests in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // Connecting Database
 const dbKey = process.env.dbKey;
