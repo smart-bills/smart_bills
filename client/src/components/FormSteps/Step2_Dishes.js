a6d64a02e16c601cb6ba5ef3af287f6774214621
WenxuC <wenx.chen@outlook.com>
2022-01-31 13:20:40 -0500

    added decimal padding

--- client/src/components/FormSteps/Step2_Dishes.js
+++ client/src/components/FormSteps/Step2_Dishes.js
@@ -1,53 +1,60 @@
 import React from 'react';
-import {TextField, Button, Container} from '@mui/material'
+import { TextField, Button, Container } from '@mui/material';
 
-function Step2_dishes({dishes, changeDishInfo, removeDish, splitBy}) {
-    
-    const showEmailField = (splitBy === 'Split by Dishes' ? true : false);
+function Step2_dishes({ dishes, changeDishInfo, removeDish, splitBy }) {
+	const showEmailField = splitBy === 'Split by Dishes' ? true : false;
 
-    return (
-    <React.Fragment>
-        {dishes.map((item, index) => (
-            <Container component='div' key={index} sx={{display: 'flex', justifyContent: 'space-evenly', p:0}}>
-                                
-                <TextField
-                    margin='normal'
-                    label="Dish Name"
-                    type='text'
-                    variant="outlined"
-                    value={item.dishName}
-                    name='dishName'
-                    onChange={e => changeDishInfo(e, index)}
-                />
-                 
-                <TextField
-                    margin='normal'
-                    label="Price"
-                    type='text'
-                    variant="outlined"
-                    value={item.amount}
-                    name='amount'
-                    onChange={e => changeDishInfo(e, index)}
-                />
+	return (
+		<React.Fragment>
+			{dishes.map((item, index) => (
+				<Container
+					component='div'
+					key={index}
+					sx={{ display: 'flex', justifyContent: 'space-evenly', p: 0 }}
+				>
+					<TextField
+						margin='normal'
+						label='Dish Name'
+						type='text'
+						variant='outlined'
+						value={item.dishName}
+						name='dishName'
+						onChange={e => changeDishInfo(e, index)}
+					/>
 
-                {showEmailField && 
-                    <TextField
-                        margin='normal'
-                        label="Email"
-                        type='text'
-                        variant="outlined"
-                        value={item.userEmail}
-                        name='userEmail'
-                        onChange={e => changeDishInfo(e, index)}
-                    />
-                }            
+					<TextField
+						margin='normal'
+						label='Price'
+						type='text'
+						variant='outlined'
+						value={item.amount}
+						name='amount'
+						onChange={e => changeDishInfo(e, index)}
+					/>
 
-                <Button onClick={e => removeDish(e, index)}>Remove</Button>
-                
-		    </Container>
-	    ))}
-    </React.Fragment>
-  )
+					{showEmailField && (
+						<TextField
+							margin='normal'
+							label='Email'
+							type='text'
+							variant='outlined'
+							value={item.userEmail}
+							name='userEmail'
+							onChange={e => changeDishInfo(e, index)}
+						/>
+					)}
+
+					<Button
+						size='small'
+						color='error'
+						onClick={e => removeDish(e, index)}
+					>
+						Remove
+					</Button>
+				</Container>
+			))}
+		</React.Fragment>
+	);
 }
 
 export default Step2_dishes;
