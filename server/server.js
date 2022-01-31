7fcbc6bb428213a93e8f9038a4a9d67325fe2071
WenxuC <wenx.chen@outlook.com>
2022-01-31 16:19:19 -0500

    setting up deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const path = require('path');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -22,5 +23,14 @@ const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log('Connected Database Successfully');
 });
+
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
